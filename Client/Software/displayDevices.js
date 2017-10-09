bd6675319c520828c4bfc54d22c639360586f467
jkt-ut <jkthai95@utexas.edu>
2017-10-09 15:02:49 -0500

    added dynamic collapsibles

--- Client/Software/displayDevices.js
+++ Client/Software/displayDevices.js
@@ -2,10 +2,25 @@ let $ = require('jquery')
 
 /*************** output the list of device names ********************/
 var cur_device_list = [];
+var cur_device_name = [];
+var cur_device_metadata = [];
+
+function metadata_str (device) {
+  return (
+    'vendorId: ' + device.vendorId + "<br \>" +
+    'productId: ' + device.productId + "<br \>" +
+    'path: ' + device.path + "<br \>" +
+    'serialNumber: ' + device.serialNumber + "<br \>" +
+    'manufacturer: ' + device.manufacturer + "<br \>" +
+    'release: ' + device.release + "<br \>" +
+    'interface: ' + device.interface
+    );
+}
 
 function getHIDdata() {
   var device_list = [];
   var display_names = [];
+  var device_metadata = [];
 
   var HID = require('./nodehid.js');
   var devices = HID.devices();
@@ -17,6 +32,7 @@ function getHIDdata() {
       if(!device_list.includes(id_string)) {
         device_list.push(id_string);
         display_names.push(devices[i].product)
+        device_metadata.push(metadata_str(devices[i]));
       }
   }
 
@@ -25,13 +41,30 @@ function getHIDdata() {
         return u === device_list[i];}))) {
       cur_device_list = device_list;
       console.log(cur_device_list)
-      $('#output').empty();
-      $('#output').append("<ul></ul>");
+      cur_device_metadata = device_metadata;
+      console.log(cur_device_metadata)
+      cur_device_name = display_names;
+      console.log(cur_device_name)
 
-      for(var i in display_names) {
+      $('#output').empty();
+
+      for(var i in cur_device_name) {
           var li = "<li>";
-          $("ul").append(li.concat(display_names[i]))
+          var p = "<p>";
+          var li_end = "</li>";
+          var p_end = "</p>";
+
+          $("#output").append(li + (cur_device_name[i]) + li_end)
+          $("#output").append(p + (cur_device_metadata[i]) + p_end)
+          $('p').hide();
+
+          console.log(cur_device_metadata[i])
       }
+      $(document).ready(function(){
+        $('li').click(function(){
+          $(this).next('p').toggle();
+        });
+      });
   }
 }
 
